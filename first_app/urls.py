5d2a1a76862bf4cccfb2936d4bf13d39278730f4
Connor Callaghan <connorcallaghan@connors-mbp.lan>
2022-11-09 10:59:35 -0800

    Update base.html, inherit it to other pages

--- first_app/urls.py
+++ first_app/urls.py
@@ -4,7 +4,8 @@ from first_app import views
 app_name = 'first_app'
 
 urlpatterns = [
-    path('',views.users,name='users'),
-    path('',views.other,name='other'),
-    path('',views.relative,name='relative')
+    path('relative/',views.relative,name='relative'),
+    path('users/',views.users,name='users'),
+    path('other/',views.other,name='other'),
+
 ]
